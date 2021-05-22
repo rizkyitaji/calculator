efc570f2a7209ccdf3d21ae7c89fbf2baed60f7b
rizkyitaji <rizkyitaji@gmail.com>
2021-05-22 11:52:52 +0700

    first commit

---
+++ lib/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color blue = Colors.blue;
+Color white = Colors.white;
+Color black = Colors.black;
+Color lightBlue = Colors.lightBlue[100];
+
+TextStyle blackFont18 =
+    TextStyle(color: black, fontSize: 18, fontWeight: FontWeight.w500);
---
+++ lib/ui/views.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:calculator/theme/theme.dart';
+
+part 'main_view.dart';
